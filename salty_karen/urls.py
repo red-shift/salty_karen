e61744b28c95f87d3dbc3b5b5091c4a553334e45
Buddy Chambers <bchambers@intelligent-d2.com>
2022-01-15 09:25:17 -0600

    Added CustomUserAdmin

--- salty_karen/urls.py
+++ salty_karen/urls.py
@@ -1,4 +1,4 @@
-"""salty_karen URL Configuration
+"""karen_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django_registration.backends.one_step.views import RegistrationView
+from app_users.forms import CustomUserForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/register/',
+        RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),
+        name='django_registration_register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('app_karen.urls')),
+    path('api/v1/', include('likes.api.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
