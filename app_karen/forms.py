cb024c300cb49735a7a0e88628adaf509f406065
Buddy Chambers <bchambers@intelligent-d2.com>
2022-04-19 09:49:37 -0500

    added description field, set video field as required

--- app_karen/forms.py
+++ app_karen/forms.py
@@ -7,12 +7,12 @@ class KarenPostForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     location = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    video = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control'}), required=False)
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))
+    video = forms.FileField(widget=forms.FileInput(), required=True)
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
 
     class Meta:
         model = KarenPost
-        fields = ['title', 'name', 'location', 'video', 'image']
+        fields = ['title', 'name', 'location', 'video', 'description']
 
 
 class UpdateCommentForm(forms.ModelForm):
